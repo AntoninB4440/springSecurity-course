d89b79566f9f23e2f2f4f5fcb6da837d039fcd44
AntoninB4440 <barceloantonin@gmail.com>
2022-02-27 16:33:14 +0100

    Ajout des getGrantedAuthorities + ajout secu sur la permission

--- src/main/java/com/antonin/springSecurity/security/config/SecurityConfig.java
+++ src/main/java/com/antonin/springSecurity/security/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.antonin.springSecurity.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +15,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.antonin.springSecurity.security.config.UserPermission.*;
 import static com.antonin.springSecurity.security.config.UserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -27,10 +32,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()//Authorize request to be made
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                /*.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())*/
                 .anyRequest() //AnyRequest made
                 .authenticated() //must be authenticated
                 .and()
@@ -43,18 +52,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                //.roles(STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                //.roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
--- src/main/java/com/antonin/springSecurity/security/config/UserRole.java
+++ src/main/java/com/antonin/springSecurity/security/config/UserRole.java
@@ -1,8 +1,11 @@
 package com.antonin.springSecurity.security.config;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.antonin.springSecurity.security.config.UserPermission.*;
 
@@ -13,15 +16,27 @@ public enum UserRole {
             STUDENT_READ,
             COURSE_READ,
             COURSE_WRITE
-    ));
+    )),
+    ADMINTRAINEE(Sets.newHashSet(
+          STUDENT_READ,
+          COURSE_READ
+          ));
 
-    private final Set<UserPermission> permissionSet;
+    private final Set<UserPermission> permissions;
 
-    UserRole(Set<UserPermission> permissionSet) {
-        this.permissionSet = permissionSet;
+    UserRole(Set<UserPermission> permissions) {
+        this.permissions = permissions;
     }
 
     public Set<UserPermission> getPermissionSet() {
-        return permissionSet;
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissionSet().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" +this.name()));
+        return permissions;
     }
 }
